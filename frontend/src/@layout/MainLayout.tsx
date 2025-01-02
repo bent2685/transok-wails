fafd552326ad19e5de0a9c658721844d92d6321d
bent2685 <bent2685@outlook.com>
2025-01-02 11:28:02 +0800

    fixbug: 修复windows顶部高度问题
    - 修复windows顶部高度问题
    - 修复文件列表底部内边距问题

--- frontend/src/@layout/MainLayout.tsx
+++ frontend/src/@layout/MainLayout.tsx
@@ -1,12 +1,21 @@
 import TitleBar from '@/components/TitleBar/TItleBar'
 import React, { useEffect, useState } from 'react'
 import RouterView from '@/routes'
+import { GetPlatform } from '@wa/services/systemService'
 interface IMainLayoutProps {}
 
 const MainLayout: React.FC<IMainLayoutProps> = props => {
+  const [isMac, setIsMac] = useState(false)
+
+  useEffect(() => {
+    GetPlatform().then(platform => {
+      setIsMac(platform === 'darwin')
+    })
+  }, [])
+
   return (
     <div className="h-full w-full flex flex-col overflow-hidden">
-      <TitleBar />
+      {isMac && <TitleBar />}
       <div className="flex flex-col flex-1 overflow-hidden px-3 py-2">
         <RouterView />
       </div>
